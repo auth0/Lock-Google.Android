b2a73b685d3ad0712f25076192b48a35598e844e
Hernan Zalazar <hernanzalazar@gmail.com>
2016-03-18 19:24:55 -0300

    Use released Lock version for permission handling

--- lock-googleplus/src/main/java/com/auth0/googleplus/GooglePlusIdentityProvider.java
+++ lock-googleplus/src/main/java/com/auth0/googleplus/GooglePlusIdentityProvider.java
@@ -24,44 +24,167 @@
 
 package com.auth0.googleplus;
 
-import android.Manifest;
+import android.app.Activity;
+import android.app.PendingIntent;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.support.v7.app.AlertDialog;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.os.Bundle;
+import android.util.Log;
 
-import com.auth0.identity.AuthorizedIdentityProvider;
+import com.auth0.core.Application;
+import com.auth0.core.Strategies;
+import com.auth0.google.FetchTokenAsyncTask;
+import com.auth0.google.R;
+import com.auth0.identity.IdentityProvider;
+import com.auth0.identity.IdentityProviderCallback;
+import com.auth0.identity.IdentityProviderRequest;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.plus.Plus;
 
-import java.util.List;
+/**
+ * Please use {@link com.auth0.google.GoogleIdentityProvider} instead that has support for Android M permission model.
+ */
+@Deprecated
+public class GooglePlusIdentityProvider implements IdentityProvider, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
-public class GooglePlusIdentityProvider extends AuthorizedIdentityProvider {
-
-    private final Context context;
+    public static final String TAG = GooglePlusIdentityProvider.class.getName();
+    private final GoogleApiClient apiClient;
+    private boolean authenticating;
+    private Activity activity;
+    private IdentityProviderCallback callback;
 
     public GooglePlusIdentityProvider(Context context) {
-        super(new GooglePlusIdentityProviderBase(context));
-        this.context = context;
+        this.apiClient = new GoogleApiClient.Builder(context)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Plus.API)
+                .addScope(Plus.SCOPE_PLUS_PROFILE)
+                .useDefaultAccount()
+                .build();
     }
 
     @Override
-    public void onPermissionsRequireExplanation(List<String> permissions) {
-        new AlertDialog.Builder(context)
-                .setTitle("Google Sign-In")
-                .setMessage("Access to Contacts is needed for this provider to work. " +
-                        "If you agree to use it, please provide access.")
-                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        retryLastPermissionRequest();
-                    }
-                })
-                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                    }
-                })
-                .setIcon(android.R.drawable.ic_dialog_email)
-                .show();
+    public void setCallback(IdentityProviderCallback callback) {
+        this.callback = callback;
     }
 
-    public String[] getRequiredPermissions() {
-        return new String[]{Manifest.permission.GET_ACCOUNTS};
+    @Override
+    public void start(Activity activity, String serviceName) {
+        this.activity = activity;
+        Log.v(TAG, "Starting G+ connection");
+        final int availabilityStatus = GooglePlayServicesUtil.isGooglePlayServicesAvailable(activity);
+        if (availabilityStatus != ConnectionResult.SUCCESS) {
+            Log.w(TAG, "Google services availability failed with status " + availabilityStatus);
+            callback.onFailure(GooglePlayServicesUtil.getErrorDialog(availabilityStatus, activity, 0));
+            stop();
+            return;
+        }
+        authenticating = true;
+        if (apiClient.isConnected()) {
+            fetchToken();
+        } else {
+            apiClient.connect();
+        }
+    }
+
+    @Override
+    public void start(Activity activity, IdentityProviderRequest event, Application application) {
+        start(activity, Strategies.GooglePlus.getName());
+    }
+
+    @Override
+    public void stop() {
+        clearSession();
+    }
+
+    @Override
+    public boolean authorize(Activity activity, int requestCode, int resultCode, Intent data) {
+        this.activity = activity;
+        if (resultCode == Activity.RESULT_CANCELED) {
+            Log.d(TAG, "User cannceled operation with code " + requestCode);
+            authenticating = false;
+            return false;
+        }
+        if (requestCode == GOOGLE_PLUS_REQUEST_CODE) {
+            Log.v(TAG, "Received request activity result " + resultCode);
+            if (!apiClient.isConnecting()) {
+                apiClient.connect();
+            }
+            return true;
+        } else if(requestCode == GOOGLE_PLUS_TOKEN_REQUEST_CODE) {
+            Log.v(TAG, "Received token request activity result " + resultCode);
+            if (!apiClient.isConnected()) {
+                apiClient.connect();
+            } else {
+                fetchToken();
+            }
+        }
+        return authenticating;
+    }
+
+    @Override
+    public void clearSession() {
+        try {
+            if (apiClient.isConnected()) {
+                Plus.AccountApi.clearDefaultAccount(apiClient);
+            }
+        } catch (IllegalStateException e) {
+            Log.e(TAG, "Failed to clear G+ Session", e);
+        } finally {
+            if (apiClient.isConnected()) {
+                apiClient.disconnect();
+            }
+            activity = null;
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        fetchToken();
+    }
+
+    @Override
+    public void onConnectionSuspended(int code) {
+        Log.v(TAG, "Connection suspended with code: " + code);
+        authenticating = false;
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        final int errorCode = result.getErrorCode();
+        Log.v(TAG, "Connection failed with code " + errorCode);
+        switch (errorCode) {
+            case ConnectionResult.SERVICE_MISSING:
+                authenticating = false;
+                Log.e(TAG, "service not available");
+                callback.onFailure(GooglePlayServicesUtil.getErrorDialog(errorCode, activity, 0));
+                break;
+            case ConnectionResult.SIGN_IN_REQUIRED:
+            case ConnectionResult.RESOLUTION_REQUIRED:
+                if (authenticating) {
+                    Log.v(TAG, "Showing G+ consent activity to the user");
+                    final PendingIntent mSignInIntent = result.getResolution();
+                    try {
+                        activity.startIntentSenderForResult(mSignInIntent.getIntentSender(), GOOGLE_PLUS_REQUEST_CODE, null, 0, 0, 0);
+                    } catch (IntentSender.SendIntentException ignore) {
+                        authenticating = false;
+                        apiClient.connect();
+                        Log.w(TAG, "G+ pending intent cancelled", ignore);
+                    }
+                }
+                break;
+            default:
+                authenticating = false;
+                Log.e(TAG, "Connection failed with unrecoverable error");
+                callback.onFailure(R.string.com_auth0_social_error_title, R.string.com_auth0_social_error_message, null);
+                break;
+        }
+    }
+
+    private void fetchToken() {
+        new FetchTokenAsyncTask(apiClient, activity, callback).execute("email", "profile");
     }
 }
