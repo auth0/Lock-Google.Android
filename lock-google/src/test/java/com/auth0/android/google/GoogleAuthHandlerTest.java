d2a26e14b7d2199609b83ced5489394a125d14ef
Hernan Zalazar <hernan.zalazar@gmail.com>
2016-09-29 16:03:23 -0300

    Clean up files

--- lock-google/src/test/java/com/auth0/android/google/GoogleAuthHandlerTest.java
+++ lock-google/src/test/java/com/auth0/android/google/GoogleAuthHandlerTest.java
@@ -11,7 +11,7 @@ import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.Matchers.nullValue;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
 public class GoogleAuthHandlerTest {
 
